6dbfff2463b2d255ccbf1f682a69f4a1b5475e61
philbowsher <indyprb@gmail.com>
2018-02-22 03:46:29 +0000

    paths

--- API/1_API_plumber/openfdafunction2.R
+++ API/1_API_plumber/openfdafunction2.R
@@ -3,5 +3,6 @@ p <- plumber::plumb(file = "plumber.R")
 rstudioapi::viewer("http://charleston.rstudio.com:8000/__swagger__/")
 p$run(port = 8000, host  = "0.0.0.0")
 
+# http://charleston.rstudio.com:8000/outcomes?drug=FUROSEMIDE
 
 
