4e1081270bb033c71a7db9f0bc9f644af4867164
edgararuiz <edgararuiz@gmail.com>
2018-04-15 10:34:53 -0500

    Adds waffle plot, finalizes look

--- Shiny/4_Shiny_Immunogenicity_MSSQL/app.R
+++ Shiny/4_Shiny_Immunogenicity_MSSQL/app.R
@@ -7,13 +7,39 @@ library(DBI)
 library(dbplot)
 library(ggplot2)
 
+
 load("samples.Rdata")
+
+# Database connection ----------------------
+# 
+# con <- dbConnect(odbc(), 
+#                  Driver = "SQL Server", 
+#                  Server = "localhost\\SQLEXPRESS", 
+#                  Database = "immunogenicity", 
+#                  Trusted_Connection = "True")
+# 
+# screening <- tbl(con, in_schema("study_01", "screening"))
+# confirmatory <- tbl(con, in_schema("study_01", "confirmatory"))
+# titer <- tbl(con, in_schema("study_01", "titer")) %>%
+#   select(- Site, - Subject)
+# subjects <- tbl(con, in_schema("study_01", "subjects"))
+# 
+# samples <- screening %>%
+#   left_join(confirmatory, by = "Sample_Number") %>%
+#   left_join(titer, by = "Sample_Number") %>%
+#   left_join(subjects, by = "Subject") %>%
+#   mutate_if(is.integer, as.numeric) %>%
+#   mutate(Signal_Response_Difference = Signal_Response_No_Drug - Signal_Response_Drug)  %>% 
+#   mutate(Signal_Response_Divide = Signal_Response_Difference / Signal_Response_No_Drug)  %>%
+#   mutate(Percent_Signal_Inhibition_Drug = Signal_Response_Divide * 100) 
+
+# Data prep for Side Menu -------------------
 
 blood <- samples %>%
   group_by(Blood_Type) %>%
   summarise() %>%
   pull()
-# Getting all ranges in one SQL "trip"
+
 ranges <- samples %>%
   summarise(
     max_weight = max(Weight, na.rm = TRUE),
@@ -25,10 +51,7 @@ ranges <- samples %>%
 
 ui <- function(request){
   dashboardPage(
-    #skin = "yellow",
-    dashboardHeader(title = "Study Results",
-                    dropdownMenuOutput("studies")
-    ),
+    dashboardHeader(title = "Study Results"),
     dashboardSidebar(
       selectInput("blood", "Blood Type:",
                   blood,
@@ -60,9 +83,10 @@ ui <- function(request){
     ),
     dashboardBody(
       fluidRow(
-        valueBoxOutput("no_samples"),
-        valueBoxOutput("true_positives"),
-        valueBoxOutput("avg_weight")
+        valueBoxOutput("no_samples", width = 3),
+        valueBoxOutput("true_positives", width = 3),
+        valueBoxOutput("avg_weight", width = 3),
+        valueBoxOutput("avg_percent", width = 3)
       ),
       fluidRow(
         box(
@@ -73,27 +97,39 @@ ui <- function(request){
                      hover = "plot_hover",
                      brush = "plot_brush"),
           title = "Signal Response Drug", 
-          width = 6, height = 320,
+          width = 4, height = 320,
           background = "light-blue"
         ),
         box(
           plotOutput("hist_no_drug", height = 250),
           title = "Signal Response No Drug", 
-          width = 6, height = 320,
+          width = 4, height = 320,
           collapsible = TRUE,
-          background = "green"
+          background = "light-blue"
+        ),
+        box(
+          plotOutput("hist_percent", height = 250),
+          title = "Percent Signal Inhibition Drug", 
+          width = 4, height = 320,
+          background = "teal"
         )
       ),
       fluidRow(
         box(
-          plotOutput("hist_percent", height = 250),
-          title = "Percent Signal Inhibition Drug", 
-          width = 6, height = 320,
-          background = "light-blue"
+          plotOutput("by_blood", height = 250),
+          title = "Samples by Blood Type", 
+          width = 3, height = 320,
+          background = "blue"
         ),
         box(
           plotOutput("hist_weight", height = 250),
-          title = "Weight", 
+          title = "Subject's Weight", 
+          width = 3, height = 320,
+          background = "purple"
+        ),
+        box(
+          plotOutput("tp_waffle", height = 250),
+          title = "Cut off results", 
           width = 6, height = 320,
           background = "green"
         )
@@ -104,6 +140,7 @@ ui <- function(request){
 
 server <- function(input, output, session) {
   
+  # Reactive master query prep ------------
   sample_data <- reactive({
     samples %>%
       filter(
@@ -121,43 +158,18 @@ server <- function(input, output, session) {
        
   })
   
-  output$hist_drug <- renderPlot({
-    sample_data() %>%
-      dbplot_histogram(Signal_Response_Drug) +
-      labs(title = "", x = "", y = "")
-  })
-  
-  output$hist_no_drug <- renderPlot({
-    sample_data() %>%
-      dbplot_histogram(Signal_Response_No_Drug) +
-      labs(title = "", x = "", y = "")
-  })
-  
-# ----------------------------
-  
-  output$hist_percent <- renderPlot({
-    sample_data() %>%
-      dbplot_histogram(Percent_Signal_Inhibition_Drug) +
-      labs(title = "", x = "", y = "")
-  })
-  
-  output$hist_weight <- renderPlot({
-    sample_data() %>%
-      dbplot_histogram(Weight) +
-      labs(title = "", x = "", y = "")
-  })
-#---------------------------
-  
-  
+
+  # Render - No of samples value box ------------
   output$no_samples <- renderValueBox({
-   valueBox(
-     sample_data() %>% 
-       tally() %>% 
-       pull(),
-     "No. of samples",
-     icon = icon("flask")
-   )})
+    valueBox(
+      sample_data() %>% 
+        tally() %>% 
+        pull(),
+      "No. of samples",
+      icon = icon("flask")
+    )})
   
+  # Render - True positives value box ------------
   output$true_positives <- renderValueBox({
     valueBox(
       sample_data() %>% 
@@ -169,6 +181,7 @@ server <- function(input, output, session) {
       color = "green"
     )})
   
+  # Render - Avg Weight value box ------------
   output$avg_weight <- renderValueBox({
     valueBox(
       sample_data() %>% 
@@ -177,8 +190,107 @@ server <- function(input, output, session) {
         round(., 0),
       "Avg. Weight",
       icon = icon("user"),
-      color = "yellow"
+      color = "purple"
     )})
+  
+  # Render - Avg percent value box ------------
+  output$avg_percent <- renderValueBox({
+    valueBox(
+      sample_data() %>% 
+        summarise(per = mean(Percent_Signal_Inhibition_Drug, na.rm = TRUE)) %>%
+        pull() %>%
+        round(., 2),
+      "Avg. Signal Percent",
+      icon = icon("percent"),
+      color = "teal"
+    )})
+  
+  # Render - Drug histogram ----------------
+  output$hist_drug <- renderPlot({
+    sample_data() %>%
+      dbplot_histogram(Signal_Response_Drug) +
+      theme(
+        axis.title.y = element_blank(),
+        axis.title.x = element_blank(),
+        title = element_blank()
+      )
+  })
+  # Render - No Drug histogram ----------------
+  output$hist_no_drug <- renderPlot({
+    sample_data() %>%
+      dbplot_histogram(Signal_Response_No_Drug)  +
+      theme(
+        axis.title.y = element_blank(),
+        axis.title.x = element_blank(),
+        title = element_blank()
+      )
+  })
+  
+  # Render - Percent histogram ----------------
+  output$hist_percent <- renderPlot({
+    sample_data() %>%
+      dbplot_histogram(Percent_Signal_Inhibition_Drug)  +
+      theme(
+        axis.title.y = element_blank(),
+        axis.title.x = element_blank(),
+        title = element_blank()
+      )
+  })
+  
+  # Render - Blood column plot ----------------
+  output$by_blood <- renderPlot({
+    sample_data() %>%
+      db_compute_count(Blood_Type) %>%
+      rename(count = `n()`) %>%
+      ggplot() +
+      geom_col(aes(Blood_Type, count, fill = count), alpha = 0.4) +
+      geom_label(aes(Blood_Type, count, label = paste0(Blood_Type, " : ", count, " samples")), label.padding = unit(0.75, "lines"), hjust = 1) +
+      coord_flip() +
+      theme_minimal() +
+      theme(
+        axis.title.y = element_blank(),
+        axis.title.x = element_blank(),
+        title = element_blank(),
+        axis.text = element_blank(),
+        panel.grid = element_blank(),
+        legend.position = "none"
+      )
+  })
+  
+  # Render - Weight histogram ----------------
+  output$hist_weight <- renderPlot({
+    sample_data() %>%
+      dbplot_histogram(Weight) +
+      theme(
+        axis.title.y = element_blank(),
+        axis.title.x = element_blank(),
+        title = element_blank()
+      )
+  })
+  
+  # Render - True positives waffle plot ----------------
+  output$tp_waffle <- renderPlot({
+    tp <- sample_data() %>% 
+      group_by(True_Positive) %>%
+      tally() %>%
+      collect() 
+    
+    parts <- c(
+      "False Positive" = pull(filter(tp, True_Positive == "No")),
+      "True Positive" = pull(filter(tp, True_Positive == "Yes"))
+      
+    )
+    
+    waffle_colors <- c("#969696", "#1879bf",  "white")
+    if(length(parts) == 1 ) waffle_colors <- "#1879bf"
+    
+    waffle(parts, rows = 5,
+           colors = waffle_colors , legend_pos = "bottom"
+    )
+  })
+  
+  
+
   
   output$studies <- renderMenu({
     msgs <- list(
@@ -194,4 +306,5 @@ server <- function(input, output, session) {
       .list = msgs)
   })
 }
+
 shinyApp(ui, server, enableBookmarking = "url")
